791452ea89c3453c14daf8693237a22b69ac8071
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-28 20:18:25 +0000

    Snapshot of branch master taken on 2019-05-28T16:18:22.869-04:00 from "https://github.com/UnofficialJuliaMirror/RigidBodyDynamics.jl-366cf18f-59d5-5db9-a4de-86a9f6786172"

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,41 @@
-using Documenter, RigidBodyDynamics, RigidBodyDynamics.OdeIntegrators
+using Documenter
+using Literate
+using RigidBodyDynamics, RigidBodyDynamics.OdeIntegrators
+
+gendir = joinpath(@__DIR__, "src", "generated")
+tutorialpages = String[]
+let
+    rm(gendir, recursive=true, force=true)
+    mkdir(gendir)
+    exampledir = joinpath(@__DIR__, "..", "examples")
+    excludedirs = String[]
+    excludefiles = String[]
+    if VERSION < v"1.1.0"
+        push!(excludefiles, "Symbolic double pendulum.jl")
+    end
+    for subdir in readdir(exampledir)
+        subdir in excludedirs && continue
+        root = joinpath(exampledir, subdir)
+        isdir(root) || continue
+        for file in readdir(root)
+            file in excludefiles && continue
+            name, ext = splitext(file)
+            lowercase(ext) == ".jl" || continue
+            outputdir = joinpath(gendir, subdir)
+            cp(root, outputdir)
+            preprocess = function (str)
+                str = replace(str, "OPEN_VISUALIZER = true" => "OPEN_VISUALIZER = false")
+                str = replace(str, "@__DIR__" => "\"$(relpath(root, outputdir))\"")
+                return str
+            end
+            absfile = joinpath(root, file)
+            # Literate.script(absfile, outputdir; preprocess=preprocess)
+            # Literate.notebook(absfile, outputdir; preprocess=preprocess)
+            tutorialpage = Literate.markdown(absfile, outputdir; preprocess=preprocess)
+            push!(tutorialpages, relpath(tutorialpage, joinpath(@__DIR__, "src")))
+        end
+    end
+end
 
 makedocs(
     modules = [RigidBodyDynamics, RigidBodyDynamics.OdeIntegrators],
@@ -8,18 +45,20 @@ makedocs(
     authors = "Twan Koolen and contributors.",
     pages = [
         "Home" => "index.md",
-        "Quick start guide" => "quickstart.md",
-        "Spatial vector algebra" => "spatial.md",
-        "Joints" => "joints.md",
-        "Rigid bodies" => "rigidbody.md",
-        "Mechanism" => "mechanism.md",
-        "MechanismState" => "mechanismstate.md",
-        "Kinematics/dynamics algorithms" => "algorithms.md",
-        "Custom collection types" => "customcollections.md",
-        "Cache types" => "caches.md",
-        "Simulation" => "simulation.md",
-        "URDF parsing and writing" => "urdf.md",
-        "Benchmarks" => "benchmarks.md"
+        "Tutorials" => tutorialpages,
+        "Library" => [
+            "Spatial vector algebra" => "spatial.md",
+            "Joints" => "joints.md",
+            "Rigid bodies" => "rigidbody.md",
+            "Mechanism" => "mechanism.md",
+            "MechanismState" => "mechanismstate.md",
+            "Kinematics/dynamics algorithms" => "algorithms.md",
+            "Custom collection types" => "customcollections.md",
+            "Cache types" => "caches.md",
+            "Simulation" => "simulation.md",
+            "URDF parsing and writing" => "urdf.md",
+        ],
+        "Benchmarks" => "benchmarks.md",
       ],
     format = Documenter.HTML(prettyurls = parse(Bool, get(ENV, "CI", "false")))
 )
